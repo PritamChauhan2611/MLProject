02fb40ea4dd8e9a68a83cfd6f2c18d571b9be9de
PritamChauhan2611 <pritamchauhan25jan@gmail.com>
2024-09-22 12:00:17 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from src.exception import CustomException
-from src.logger import logging
+from src.logger import logging  
 import pandas as pd
 
 from sklearn.model_selection import train_test_split # type: ignore
@@ -25,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv("D:/New folder (2)/stud.csv")
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
